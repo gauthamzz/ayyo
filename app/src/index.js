ff25a1b0febc19fcccfd95b98b939e07aa6fbed0
gauthamzz <thabeatsz@gmail.com>
2019-03-10 09:24:57 +0100

    make release

--- app/src/index.js
+++ app/src/index.js
@@ -1,37 +1,11 @@
 // <meta name="restrictedextras" content="add id over here" />
 
 import Web3 from "web3";
-import creatorCoinArtifact from "../../build/contracts/Creator.json";
-
-function getMeta(metaName) {
-  const metas = document.getElementsByTagName('meta');
-  for (let i = 0; i < metas.length; i++) {
-    if (metas[i].getAttribute('name') === metaName) {
-      return metas[i].getAttribute('content');
-    }
-  }
-
-  return '';
+import creatorCoinArtifact from "../../build/contracts/CreatorFactory.json";
+export function displayPaywallCode(){
+  document.getElementById("paywallcode").style.display = "block";
+  document.getElementById("paywall").style.display = "none";
 }
-
-function addImageOnTop(element){
-  var rect = element.getBoundingClientRect();
-  var width = element.offsetWidth;
-  var height = element.offsetHeight;
-  console.log(rect.top, rect.right, rect.bottom, rect.left);
-  console.log(width,height);
-  let content = '<div id="payforviewplaceholder" style=" position:absolute;left:'+ Math.floor(rect.left)+'px;top:'+Math.floor(rect.top)+'px;"> \
-  <img  src="https://via.placeholder.com/'+Math.floor(rect.width)+'x'+Math.floor(rect.height)+'/f7f7f7/000?text=Premium+Content+Click+to+Pay" onclick="App.payContent()" style="box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);;z-index:999">\
-  </div>'
-  document.body.innerHTML = document.body.innerHTML + content;
-}
-function hide(elements) {
-  elements = elements.length ? elements : [elements];
-  for (var index = 0; index < elements.length; index++) {
-    elements[index].style.display = "none";
-  }
-}
-
 
 const App = {
   web3: null,
@@ -44,16 +18,12 @@ const App = {
     try {
       // get contract instance
       const networkId = await web3.eth.net.getId();
-      let addressOfContract = getMeta('contractAddress');
-      console.log("Using contract address at " + addressOfContract)
-      if(!addressOfContract){
+      // const networkType = web3.currentProvider.connection._url;
+      // console.log(networkType)
       const deployedNetwork = creatorCoinArtifact.networks[networkId];
-      addressOfContract = deployedNetwork.address;
-      }
-      console.log("Using contract address at " + addressOfContract)
       this.meta = new web3.eth.Contract(
         creatorCoinArtifact.abi,
-        addressOfContract
+        deployedNetwork.address
       );
 
       // get accounts
@@ -67,54 +37,59 @@ const App = {
   },
 
   refreshBalance: async function() {
-    const { balanceOf } = this.meta.methods;
-    const balance = await balanceOf(this.account).call();
-    console.log("balance is" + balance)
+    const { getDeployedCreators } = this.meta.methods;
+    const balance = await getDeployedCreators().call();
 
     const balanceElement = document.getElementsByClassName("balance")[0];
+    console.log(balance);
+    var content = ""
+    balance.forEach(function(entry) {
+      content = content.concat('\
+      <div class="card"> \
+  <div class="card-content has-text-centered"> \
+    <p class="subtitle"> \
+     '+entry+'\
+    </p>\
+  </div> <hr> \
+  <p class=" has-text-centered is-size-4">To add paywall copy paste this code in your HTML file </p><div class="box" style="margin:3%"> \
+  <xmp id="paywallcode" style="" class="is-family-code is-size-7"> <meta name="contractAddress" content="'+ entry+'" />  </xmp> \
+     <xmp id="paywallcode" style="" class="is-family-code is-size-7"> <script src="https://raw.githubusercontent.com/gauthamzz/ayyo/master/serve/paywall.js"></script> </xmp> \
+    </div>  <hr> <p class="is-size-4 has-text-centered">Add Premium Content </p>\
+    <div class="box" style="margin:3%"> <xmp class="is-family-code is-size-7"> \
+      <meta name="restrictedextras" content="id of content you want to hide" />  </xmp> \
+      <xmp class="is-family-code is-size-7"> <script src="https://raw.githubusercontent.com/gauthamzz/ayyo/master/serve/hide.js"></script> \
+      </xmp> </div>\
+</div> <br>\
+     ' )})
+    
 
-    if (balance == 0) {
-      let divToHide = getMeta('restrictedextras');
-      let element = document.getElementById(divToHide);
-      addImageOnTop(element);
-
-
-    } else {
-      var paywall = document.getElementById("payforviewplaceholder");
-      if (paywall) {
-        hide(document.getElementById("payforviewplaceholder"));
-      }
-     
-    }
-    // balanceElement.innerHTML = balance;
+    balanceElement.innerHTML = content;
   },
 
-  payContent: async function() {
-    // const amount = parseInt(document.getElementById("amount").value);
-    const { getPrice } = this.meta.methods;
-    let amount = await getPrice().call();
-    const { getLength } = this.meta.methods;
-    let lengthOfTokens = await getLength().call();
-    
-    amount = parseInt(amount) + parseFloat(0.001*lengthOfTokens);
-    console.log(amount)
+  createCreator: async function() {
+    const expiration = parseInt(document.getElementById("expiration").value);
+    const price = parseInt(document.getElementById("price").value);
 
-    // this.setStatus("Initiating transaction... (please wait)");
-
-    const { payContent } = this.meta.methods;
-    await payContent().send({
-      value: web3.toWei(amount, "ether"),
+    const { createCreator } = this.meta.methods;
+   
+    // skale 
+    await createCreator(expiration, price).send({
       from: this.account,
-      gas: "1000000"
+      gas: 8000000
     });
+
+    // // localhost
+    // await createCreator(expiration, price).send({
+    //   from: this.account
+    // });
 
     // this.setStatus("Transaction complete!");
     this.refreshBalance();
   },
 
   setStatus: function(message) {
-    // const status = document.getElementById("status");
-    // status.innerHTML = message;
+    const status = document.getElementById("status");
+    status.innerHTML = message;
   }
 };
 
